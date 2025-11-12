961a17e12d216dea15ad94d58bd1732c41d85bb1
Amirreza Rashvand <rashvandamirreza19@gmail.com>
2025-11-12 12:12:34 +0330

    modal for filter design done

--- app/(agent)/agent/components/tickets/TicketsHead.js
+++ app/(agent)/agent/components/tickets/TicketsHead.js
@@ -1,10 +1,20 @@
+"use client";
+
 import Link from "next/link";
+import { useState } from "react";
+import Modal from "../Modal";
 
 const TicketsHead = () => {
+  const [openFilter, setOpenFilter] = useState(false);
+
   return (
     <div className="flex items-center justify-between">
       <div className="flex items-center gap-x-[5px]">
-        <button className="flex items-center justify-center w-[35px] h-[35px] bg-[#EFF0F6] rounded-[10px]">
+        <button
+          type="button"
+          className="flex items-center justify-center w-[35px] h-[35px] bg-[#EFF0F6] rounded-[10px]"
+          onClick={() => setOpenFilter((value) => !value)}
+        >
           <svg className="w-5 h-5">
             <use href="#filter" />
           </svg>
@@ -22,6 +32,45 @@ const TicketsHead = () => {
           <use href="#arrow-narrow-left" />
         </svg>
       </Link>
+
+      {openFilter && (
+        <Modal>
+          <h3 className="text-[#404040] font-semibold leading-[22.4px] tracking-[-0.12px]">
+            اعمال فیلتــــر
+          </h3>
+
+          <div className="flex flex-col gap-y-6 mt-6">
+            <button className="w-full py-3 bg-[#EFF0F6] rounded-[10px] font-medium leading-6 text-[#404040]">
+              جدیدترین ها
+            </button>
+            <button className="w-full py-3 bg-[#EFF0F6] rounded-[10px] font-medium leading-6 text-[#404040]">
+              قدیمی ترین
+            </button>
+            <button className="w-full py-3 bg-[#EFF0F6] rounded-[10px] font-medium leading-6 text-[#404040]">
+              دارای SLA
+            </button>
+            <button className="w-full py-3 bg-[#EFF0F6] rounded-[10px] font-medium leading-6 text-[#404040]">
+              انتخاب تاریخ
+            </button>
+            <button className="w-full py-3 bg-[#EFF0F6] rounded-[10px] font-medium leading-6 text-[#404040]">
+              انتخاب ساعت
+            </button>
+            <button className="w-full py-3 bg-[#EFF0F6] rounded-[10px] font-medium leading-6 text-[#404040]">
+              انتخاب کاربر
+            </button>
+
+            <button className="w-full py-3 bg-[#20CFCF] text-white rounded-[10px] font-medium leading-6">
+              اعمال فیلتر
+            </button>
+            <button
+              className="w-full text-[#6C7278] font-medium leading-6 text-xs"
+              onClick={() => setOpenFilter((value) => !value)}
+            >
+              منصرف شدم
+            </button>
+          </div>
+        </Modal>
+      )}
     </div>
   );
 };
