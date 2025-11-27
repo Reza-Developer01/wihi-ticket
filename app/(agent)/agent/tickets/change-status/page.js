514c6f12bb001e7ea50990b93449456930db81ca
Amirreza Rashvand <rashvandamirreza19@gmail.com>
2025-11-27 22:24:47 +0330

    use Suspense to agent/tickets/change-status

--- app/(agent)/agent/tickets/change-status/page.js
+++ app/(agent)/agent/tickets/change-status/page.js
@@ -1,25 +1,25 @@
 "use client";
 
+import { Suspense } from "react";
 import { useSearchParams } from "next/navigation";
 import AgentBottomPage from "../../components/AgentBottomPage";
 import Header from "@/app/(main)/components/Header";
 import SuccessfullyStatus from "../../components/SuccessfullyStatus";
 
-const page = () => {
+const ChangeStatusPage = () => {
   const searchParams = useSearchParams();
   const ticketNumber = searchParams.get("ticket");
 
   return (
-    <>
+    <Suspense fallback={<div>Loading...</div>}>
       <Header showBackButton={true} />
-
       <AgentBottomPage pb="pb-[49px]">
         <div className="container">
           <SuccessfullyStatus ticketNumber={ticketNumber} />
         </div>
       </AgentBottomPage>
-    </>
+    </Suspense>
   );
 };
 
-export default page;
+export default ChangeStatusPage;
