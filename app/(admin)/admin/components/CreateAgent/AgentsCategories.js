952df845b7bdbea7b1550456c600ab21e433f0ee
Amirreza Rashvand <rashvandamirreza19@gmail.com>
2025-11-27 15:52:03 +0330

    append plus icon for button

--- app/(admin)/admin/components/CreateAgent/AgentsCategories.js
+++ app/(admin)/admin/components/CreateAgent/AgentsCategories.js
@@ -175,7 +175,20 @@ const AgentsCategories = ({ agentsCategory }) => {
                 }}
                 className="flex items-center justify-center w-[62px] h-full border-r border-r-[#EDF1F3] text-xs"
               >
-                Add
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="1.5"
+                  stroke="currentColor"
+                  class="size-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M12 4.5v15m7.5-7.5h-15"
+                  />
+                </svg>
               </button>
             </div>
 
