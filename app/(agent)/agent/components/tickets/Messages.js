3b8e3f780896a1cc160682cb89b4b99d9ea6b05d
Amirreza Rashvand <rashvandamirreza19@gmail.com>
2025-11-30 17:02:52 +0330

    handle chat page agent

--- app/(agent)/agent/components/tickets/Messages.js
+++ app/(agent)/agent/components/tickets/Messages.js
@@ -29,8 +29,32 @@ const Messages = ({ request, getTicketHistory, status }) => {
         };
     }
   };
-
   const { bg, text, message } = getStatusStyle(request.status);
+
+  const allMessages = [
+    {
+      id: "first_message",
+      message: request.first_message,
+      sender_name: request.owner_name,
+      created_at: request.created_at,
+      file: request.file,
+    },
+    ...request.messages,
+  ];
+
+  const getMessageStyle = (sender_name, owner_name) => {
+    if (sender_name === owner_name) {
+      return {
+        bg: "bg-white",
+        align: "self-end",
+      };
+    } else {
+      return {
+        bg: "bg-[#EFF0F6]",
+        align: "self-start",
+      };
+    }
+  };
 
   return (
     <div className="relative h-[520px]">
@@ -44,73 +68,84 @@ const Messages = ({ request, getTicketHistory, status }) => {
 
       {/* body */}
       <div className="flex flex-col overflow-y-auto gap-y-[25px] h-[calc(100%-54px)]">
-        {/* item */}
-        {request.messages.map((item) => (
-          <div key={item.id} className="flex flex-col gap-y-2.5 *:w-[245px]">
-            {/* text */}
-            {item.file ? (
-              <>
-                <div className="custom-shadow flex items-center h-12 pr-2.5 bg-white border border-[#F1F1F7] rounded-[10px]">
-                  <p className="text-[#404040] text-xs/[16.8px] w-[202px]">
+        {allMessages.map((item) => {
+          const { bg, text, align } = getMessageStyle(
+            item.sender_name,
+            request.owner_name
+          );
+
+          return (
+            <div
+              key={item.id}
+              className={`flex flex-col gap-y-2.5 ${align} *:w-[245px]`}
+            >
+              {item.file ? (
+                <>
+                  <div
+                    className={`custom-shadow flex items-center h-12 pr-2.5 ${bg} border border-[#F1F1F7] rounded-[10px]`}
+                  >
+                    <p className={`${text} text-xs/[16.8px] w-[202px]`}>
+                      {item.message}
+                    </p>
+                  </div>
+                  <div
+                    className={`flex items-center ${
+                      align === "self-end" ? "justify-start" : "justify-end"
+                    }`}
+                  >
+                    <button
+                      type="button"
+                      className="flex flex-col items-center justify-center w-[45px] h-[45px] border border-[#808392] rounded-[10px]"
+                    >
+                      <svg className="w-6 h-6 text-[#808392]">
+                        <use href="#paper-download" />
+                      </svg>
+                      <span className="text-[#808392] text-[7px]/[9.8px] tracking-[-0.12px]">
+                        {item.file
+                          ? item.file
+                              .split("/")
+                              .pop()
+                              .split(".")
+                              .pop()
+                              .toUpperCase()
+                          : ""}
+                      </span>
+                    </button>
+                  </div>
+                </>
+              ) : (
+                <div
+                  className={`custom-shadow flex items-center h-12 pr-2.5 ${bg} border border-[#F1F1F7] rounded-[10px]`}
+                >
+                  <p className={`${text} text-xs/[16.8px] w-[202px]`}>
                     {item.message}
                   </p>
                 </div>
-                <div className="flex items-center justify-end">
-                  <button
-                    type="button"
-                    className="flex flex-col items-center justify-center w-[45px] h-[45px] border border-[#808392] rounded-[10px]"
-                  >
-                    <svg className="w-6 h-6 text-[#808392]">
-                      <use href="#paper-download" />
-                    </svg>
-                    <span className="text-[#808392] text-[7px]/[9.8px] tracking-[-0.12px]">
-                      {item.file
-                        ? item.file
-                            .split("/")
-                            .pop()
-                            .split(".")
-                            .pop()
-                            .toUpperCase()
-                        : ""}
-                    </span>
-                  </button>
+              )}
+
+              <div className="flex items-center justify-between text-[#8C8C8C]">
+                <div className="flex items-center gap-x-1">
+                  <svg className="w-2.5 h-2.5">
+                    <use href="#profile" />
+                  </svg>
+                  <span className="font-light text-[10px]/[14px] tracking-[-0.12px]">
+                    {item.sender_name}
+                  </span>
                 </div>
-              </>
-            ) : (
-              <div className="custom-shadow flex items-center h-12 pr-2.5 bg-white border border-[#F1F1F7] rounded-[10px]">
-                <p className="text-[#404040] text-xs/[16.8px] w-[202px]">
-                  {item.message}
-                </p>
-              </div>
-            )}
-
-            {/* info */}
-            <div className="flex items-center justify-between text-[#8C8C8C]">
-              {/* right */}
-              <div className="flex items-center gap-x-1">
-                <svg className="w-2.5 h-2.5">
-                  <use href="#profile" />
-                </svg>
-                <span className="font-light text-[10px]/[14px] tracking-[-0.12px]">
-                  {item.sender_name}
-                </span>
-              </div>
-
-              <div className="flex items-center gap-x-1 font-light text-[10px]/[14px] tracking-[-0.12px]">
-                <span>
-                  {new Date(item.created_at).toLocaleTimeString("fa-IR")}
-                </span>
-                <span>-</span>
-                <span>
-                  {new Date(item.created_at).toLocaleDateString("fa-IR")}
-                </span>
+                <div className="flex items-center gap-x-1 font-light text-[10px]/[14px] tracking-[-0.12px]">
+                  <span>
+                    {new Date(item.created_at).toLocaleTimeString("fa-IR")}
+                  </span>
+                  <span>-</span>
+                  <span>
+                    {new Date(item.created_at).toLocaleDateString("fa-IR")}
+                  </span>
+                </div>
               </div>
             </div>
-          </div>
-        ))}
+          );
+        })}
       </div>
-
-      {/* <HistoryStatus /> */}
     </div>
   );
 };
