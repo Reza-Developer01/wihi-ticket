c7e972dbebe51a9127b0e5121f59ed95c9e50679
Amirreza Rashvand <rashvandamirreza19@gmail.com>
2025-12-13 18:57:26 +0330

    change API

--- app/(main)/call-request/page.js
+++ app/(main)/call-request/page.js
@@ -15,8 +15,8 @@ const page = async () => {
 
   const headers = token ? { Authorization: `Bearer ${token}` } : undefined;
 
-  const categories = await getFetch("category-callrequests/", headers);
-  const services = await getFetch("service-callrequests/", headers);
+  const categories = await getFetch("users/agents-categories/", headers);
+  const services = await getFetch("service-tickets/", headers);
   const phones = await getFetch(`users/phones/`, headers);
 
   return (
