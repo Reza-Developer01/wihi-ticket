504e90c972e85aa207bee9dfce09e6fe20f9fffa
Amirreza Rashvand <rashvandamirreza19@gmail.com>
2025-12-23 01:59:31 +0330

    back button for create request and call done

--- app/(main)/components/BackButton.js
+++ app/(main)/components/BackButton.js
@@ -1,12 +1,27 @@
 "use client";
 
-import { useRouter } from "next/navigation";
+import { useRouter, usePathname } from "next/navigation";
 
 const BackButton = () => {
   const router = useRouter();
+  const pathname = usePathname();
+  const url = pathname;
+  const parts = url.split("/");
+  const result = parts.slice(2).join("/");
+
+  const handleBack = () => {
+    switch (pathname) {
+      case `/requests-list/${result}`:
+        return router.replace("/requests-list");
+      case `/call-request/${result}`:
+        return router.replace("/call");
+    }
+
+    router.back();
+  };
 
   return (
-    <button onClick={() => router.back()}>
+    <button onClick={handleBack}>
       <svg className="w-6 h-6 text-white">
         <use href="#arrow-narrow-left" />
       </svg>
